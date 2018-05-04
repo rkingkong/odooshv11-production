ad332701b38e2d08fe9192eed25dc51f8f90b9d6
Cybroodoo <odoo@cybrosys.com>
2018-05-04 18:37:48 +0530

    [INIT]Initial Commit

---
+++ prime_doors_picking_operation/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Prime Doors Picking Operation',
+    'version': '11.0.1.0.0',
+    'summary': '',
+    'category': 'Inventory',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['base', 'account_invoicing', 'stock', 'sale_management', 'prime_doors_external_layout'],
+    'website': 'https://www.cybrosys.com',
+    'data': [
+            'views/stock_picking_view.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
