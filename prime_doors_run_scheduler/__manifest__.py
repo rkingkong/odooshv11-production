ea47e8e88baaccb6f88d9af77c932136870db570
Cybroodoo <odoo@cybrosys.com>
2018-05-16 10:45:47 +0530

    [INIT]initial commit

---
+++ prime_doors_run_scheduler/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Prime Doors Run Scheduler',
+    'version': '11.0.1.0.0',
+    'summary': 'This module create Run Scheduler Button on Sales menu',
+    'category': 'Sales',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'depends': ['base', 'stock', 'sale_management'],
+    'website': 'https://www.cybrosys.com',
+    'data': [
+        'views/sales_run_scheduler_button.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
