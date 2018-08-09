1e09900c297dff0d7934dff2bdecea3ae11dc9d1
Neha Gupta <neha.gupta453@webkul.com>
2018-08-09 11:03:02 +0530

    fix order line sequence issue

--- wk_add_delivery_columns/models/delivery.py
+++ wk_add_delivery_columns/models/delivery.py
@@ -21,11 +21,31 @@ class StockMove(models.Model):
         for obj in self:
             obj.purchase_order_id = self.env['purchase.order.line'].search([('product_id','=',obj.product_id.id)], limit=1, order="id desc").order_id.id
 
+
+class SaleOrder(models.Model):
+    
+    _inherit = 'sale.order'
+
+    @api.multi
+    def _reset_sequence(self):
+        for rec in self:
+            current_sequence = 1
+            for line in rec.order_line:
+                line.write({'sequence': current_sequence})
+                current_sequence += 1
+
+
 class SaleOrderLine(models.Model):
     
     _inherit ="sale.order.line"
 
     purchase_order_id = fields.Many2one('purchase.order',
         'Purchase Order', ondelete='set null', related="move_ids.purchase_order_id", store=True, index=True, readonly=True, copy=False)
+
+    @api.model
+    def create(self, vals):
+        res = super(SaleOrderLine, self).create(vals)
+        res.order_id._reset_sequence()
+        return res
         
         
